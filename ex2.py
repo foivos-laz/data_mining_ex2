2daa0b966929bfe3c85f130ca580c50ded901c15
foivos-laz <foivoslazaridis@gmail.com>
2025-12-27 15:02:54 +0200

    second commit

--- ex2.py
+++ ex2.py
@@ -2,3 +2,20 @@ import pandas as pd
 
 movies = pd.read_csv('movies.csv')
 ratings = pd.read_csv('ratings.csv')
+
+unique_genres = (
+    movies["genres"]
+    .dropna()
+    .str.split("|")
+    .explode()
+    .str.strip()
+    .unique()
+)
+
+nr_unique_genres = unique_genres.size
+
+users = []
+
+print(ratings["userId"].max())
+
+# for i in range(1, ratings["userId"].max(), 1):
